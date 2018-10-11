a590beeac81ffadc53c16ebb0849201850e1b35d
Matt Etress <mattetress@me.com>
2018-10-11 23:36:50 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,3 +9,7 @@ def create_project_hash
   # write your code here
 end
 binding.pry
+create_project_hash
+
+# projects: kickstarter.css(li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
